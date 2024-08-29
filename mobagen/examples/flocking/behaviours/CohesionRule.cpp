12c8d60749fdaa7ace4036ffc6df78f0e3f07a72
Anne <112440960+AKonicki26@users.noreply.github.com>
2024-08-29 10:52:05 -0400

    Finish Cohesion Rule

--- mobagen/examples/flocking/behaviours/CohesionRule.cpp
+++ mobagen/examples/flocking/behaviours/CohesionRule.cpp
@@ -1,6 +1,8 @@
 #include "CohesionRule.h"
 #include "../gameobjects/Boid.h"
 
+#include <iostream>
+
 Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   Vector2f cohesionForce;
 
@@ -8,6 +10,15 @@ Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid
   // hint: iterate over the neighborhood
 
   // find center of mass
+
+  Vector2f sumOfPositions;
+
+  for (Boid* neighbor : neighborhood)
+    sumOfPositions += neighbor->getPosition();
+
+  Vector2f centerOfMass = sumOfPositions / neighborhood.size();
+
+  cohesionForce = (centerOfMass - boid->getPosition()) / boid->getDetectionRadius();
 
   return cohesionForce;
 }
