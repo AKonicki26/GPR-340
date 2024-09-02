a063687ad3d769af39ae3736b00f8b8ee091b013
Anne <112440960+AKonicki26@users.noreply.github.com>
2024-09-02 10:06:53 -0400

    Separation Exists but doesn't work

--- mobagen/examples/flocking/behaviours/SeparationRule.cpp
+++ mobagen/examples/flocking/behaviours/SeparationRule.cpp
@@ -7,16 +7,26 @@ Vector2f SeparationRule::computeForce(const std::vector<Boid*>& neighborhood, Bo
   // Try to avoid boids too close
   Vector2f separatingForce = Vector2f::zero();
 
-    float desiredDistance = desiredMinimalDistance;
+  float desiredDistance = desiredMinimalDistance;
 
-    // todo: implement a force that if neighbor(s) enter the radius, moves the boid away from it/them
-    if (!neighborhood.empty()) {
-        Vector2f position = boid->transform.position;
-        int countCloseFlockmates = 0;
-        // todo: find and apply force only on the closest mates
+  if (neighborhood.empty()) return Vector2f::zero();
+
+  // todo: implement a force that if neighbor(s) enter the radius, moves the boid away from it/them
+
+  Vector2f position = boid->transform.position;
+  int countCloseFlockmates = 0;
+  // todo: find and apply force only on the closest mates
+
+
+  for (Boid* neightbor : neighborhood) {
+    Vector2f seperationVector = neightbor->getPosition() - boid->getPosition();
+
+    if (seperationVector.getMagnitude() <= desiredDistance && seperationVector.getMagnitude() > 0) {
+      separatingForce += Vector2f::normalized(seperationVector) / seperationVector.getMagnitude();
     }
+  }
 
-  separatingForce = Vector2f::normalized(separatingForce);
+  //separatingForce = Vector2f::normalized(separatingForce);
 
   return separatingForce;
 }
