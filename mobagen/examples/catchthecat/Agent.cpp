b8bddde71790cda0c06e0449c7f980b5504e8f7d
Anne <112440960+AKonicki26@users.noreply.github.com>
2024-10-10 11:03:19 -0400

    Dijkstra's rework done

--- mobagen/examples/catchthecat/Agent.cpp
+++ mobagen/examples/catchthecat/Agent.cpp
@@ -5,17 +5,31 @@
 #include "World.h"
 
 #include <functional>
+
+#define Weight int
+
+struct WeightCompare {
+  bool operator()(const std::pair<Point2D, Weight>& first, const std::pair<Point2D, Weight>& second) {
+    return first.second > second.second;
+  }
+};
+
 using namespace std;
 std::vector<Point2D> Agent::generatePath(World* w) {
   unordered_map<Point2D, Point2D> cameFrom;  // to build the flowfield and build the path
-  queue<Point2D> frontier;                   // to store next ones to visit
+
+  priority_queue<std::pair<Point2D, Weight>, vector<std::pair<Point2D, Weight>>, WeightCompare> frontier;
+
+  //queue<Point2D> frontier;                   // to store next ones to visit
   unordered_set<Point2D> frontierSet;        // OPTIMIZATION to check faster if a point is in the queue
   unordered_map<Point2D, bool> visited;      // use .at() to get data, if the element dont exist [] will give you wrong results
+  unordered_map<Point2D, Weight> weights;
 
   // bootstrap state
   auto catPos = w->getCat();
-  frontier.push(catPos);
+  frontier.push(std::make_pair(catPos, 0));
   frontierSet.insert(catPos);
+  weights[catPos] = 0;
   Point2D borderExit = Point2D::INFINITE;  // if at the end of the loop we dont find a border, we have to return random points
 
   auto getVistableNeighbors = [&](World* world, Point2D point)  -> std::vector<Point2D> {
@@ -53,22 +67,26 @@ std::vector<Point2D> Agent::generatePath(World* w) {
 
   while (!frontier.empty() && !exitFound) {
     // get the current from frontier
-    Point2D const current = frontier.front();
+    std::pair<Point2D, Weight> const current = frontier.top();
     frontier.pop();
     // remove the current from frontierSet
-    frontierSet.erase(current);
-    if (visited.contains(current)) {
+    frontierSet.erase(current.first);
+    if (visited.contains(current.first)) {
       continue;
     }
+
+    const auto currentPoint = current.first;
+    const auto currentWeight = current.second;
+
     // mark current as visited
-    visited[current] = true;
+    visited[currentPoint] = true;
     // getVisitableNeightbors(world, current) returns a vector of neighbors that are not visited, not cat, not block, not in the queue
-    std::vector<Point2D> const neighbors = getVistableNeighbors(w, current);
+    std::vector<Point2D> const neighbors = getVistableNeighbors(w, currentPoint);
     // iterate over the neighs:
 
     for (auto neighbor : neighbors) {
     // for every neighbor set the cameFrom
-      cameFrom[neighbor] = current;
+      cameFrom[neighbor] = currentPoint;
 
       // Neighbor exists on the border
       if (abs(neighbor.x) == w->getWorldSideSize() / 2 || abs(neighbor.y) == w->getWorldSideSize() / 2) {
@@ -77,7 +95,7 @@ std::vector<Point2D> Agent::generatePath(World* w) {
         break;
       }
     // enqueue the neighbors to frontier and frontierSet
-      frontier.push(neighbor);
+      frontier.push(std::make_pair(neighbor, currentWeight + 1));
       frontierSet.insert(neighbor);
     }
     // do this up to find a visitable border and break the loop
@@ -97,3 +115,5 @@ std::vector<Point2D> Agent::generatePath(World* w) {
   // if your vector is filled from the border to the cat, the first element is the catcher move, and the last element is the cat move
   return path;
 }
+
+
